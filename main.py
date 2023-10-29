b098738884be2757de03922c1275596d9b8de311
Pola Borkowska <poolaborkowska@gmail.com>
2023-10-29 14:48:06 +0100

    another stat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 from math import sqrt, pi, cos, sin
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind, shapiro, levene, mannwhitneyu
 
 class Dartboard_1:
     
@@ -14,7 +14,7 @@ class Dartboard_1:
         y = random.uniform(-self.radiusOfBoard,self.radiusOfBoard)
         coordinates = (x,y)
         distanceFrom0 = sqrt(x**2+y**2)
-        
+        print(distanceFrom0)
         if distanceFrom0 > self.radiusOfBoard:
             score = 0
         elif distanceFrom0 < (self.radiusOfBoard/10):
@@ -22,11 +22,12 @@ class Dartboard_1:
         elif distanceFrom0 >= (self.radiusOfBoard/10) and distanceFrom0 < (self.radiusOfBoard/5):
             score = 4
         elif distanceFrom0 >= (self.radiusOfBoard/5) and distanceFrom0 < (self.radiusOfBoard/2):
-            score = 3    
+            score = 3  
         elif distanceFrom0 >= (self.radiusOfBoard/2) and distanceFrom0 < (self.radiusOfBoard/1.25):
             score = 2
         else:
             score = 1
+            
         self.save_score(coordinates, score)  
     
     def save_score(self, coordinates, score):
@@ -47,13 +48,15 @@ class Dartboard_2:
         self.radiusOfBoard = 0.5
     
     def throw(self):
-        r = random.uniform(0,1/sqrt(pi))
+        a = self.radiusOfBoard/0.5
+        
+        r = random.uniform(0,a * 1/sqrt(pi))
         phi = random.uniform(0,2*pi)
         x = r*cos(phi)
         y = r*sin(phi)
         coordinates = (x,y)
         distanceFrom0 = sqrt(x**2+y**2)
-        
+        print(distanceFrom0)
         if distanceFrom0 > self.radiusOfBoard:
             score = 0
         elif distanceFrom0 < (self.radiusOfBoard/10):
@@ -77,17 +80,20 @@ class Dartboard_2:
             self.throw()
         
         self.averageScore = sum(self.scores)/nrOfRounds
-        print(self.historyOfCoordinates)
-        print(self.scores)
             
-dartss = Dartboard_1()
-dartss.simulate(200)
-d = Dartboard_2()
-d.simulate(200)
 
-def test_equal_means(firstSample : Dartboard_1, secondSample : Dartboard_2, alpha):
+
+def test_equal_means(firstSample : Dartboard_1, secondSample : Dartboard_2, alpha = 0.05):
     #h0 -> mean of scores from dartboard_1 = mean of scores from dartboard_2
-    t, p = ttest_ind(firstSample.scores, secondSample.scores)
+    
+    stat1, p1 = shapiro(firstSample.scores)
+    stat2, p2 = shapiro(secondSample.scores)
+    statLevene, pLevene = levene(firstSample.scores, secondSample.scores)
+    
+    if p1 > 0.05 and p2 > 0.05 and pLevene > 0.05:
+        stat, p = ttest_ind(firstSample.scores, secondSample.scores)
+    else:
+        stat, p = mannwhitneyu(firstSample.scores, secondSample.scores)
     
     if p > alpha:
         #there is no reason to reject h0
@@ -95,6 +101,11 @@ def test_equal_means(firstSample : Dartboard_1, secondSample : Dartboard_2, alph
         print(f'Average scores: dartboard 1 = {firstSample.averageScore}, dartboard 2 = {secondSample.averageScore}')
         
     else:
-        print(f"There is a significant difference between average scores of dartboard 1 and dartboard 2. We reject h0 at a significance level of {p}.")
+        print(f"There is a significant difference between average scores of dartboard 1 and dartboard 2. We reject h0 at a significance level of {alpha}.")
         print(f'Average scores: dartboard 1 = {firstSample.averageScore}, dartboard 2 = {secondSample.averageScore}')
-    
+
+playFirstDartboard = Dartboard_1()
+playFirstDartboard.simulate(200)
+playSecondDartboard = Dartboard_2()
+playSecondDartboard.simulate(200)
+test_equal_means(playFirstDartboard,playSecondDartboard)
